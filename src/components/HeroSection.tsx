441c02d0f07253f7ef049cb6576035b07f73518d
VaibhavValari <vaibhav@valarisolutions.com>
2023-10-11 23:40:02 +0530

    chore: add hero backdrop blur objs

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { OrbitControls } from '@react-three/drei';
 
 const HeroSection: FC = () => {
   return (
-    <section className="p-5 md:p-20 flex gap-10 flex-wrap items-center">
+    <section className="p-5 md:p-20 flex gap-10 flex-wrap items-center relative">
       <div className="max-w-max md:max-w-min">
         <h1 className="heading whitespace-pre leading-tight font-semibold text-center">
           Software Engineer
@@ -15,6 +15,15 @@ const HeroSection: FC = () => {
           experiences that not only look good but function flawlessly.
         </p>
       </div>
+      <div
+        className={`bg-[#3ABFF860] absolute top-0 left-[10%] w-[5vw] h-[5vw] rounded blur-xl z-[-1]`}
+      />
+      <div
+        className={`bg-[#db277760] absolute top-[85%] w-[10vw] h-[10vw] rounded-full blur-2xl z-[-1]`}
+      />
+      <div
+        className={`bg-[#875FFA50] absolute top-0 left-[45%] w-[20vw] h-[20vw] rounded-full blur-2xl z-[-1]`}
+      />
 
       <div className="flex flex-1 justify-center items-center">
         <div className="avatar">
